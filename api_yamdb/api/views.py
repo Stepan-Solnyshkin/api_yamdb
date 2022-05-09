41366151bc8dd41e3cc5df8ac913bae57a217dea
Dmitry Permyakov <taren4ik@mail.ru>
2022-05-09 23:05:32 +1000

    Создал шаблоны viewset.

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -7,3 +7,13 @@ from .serializers import (CommentSerializer, ReviewSerializer)
 class TitleViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Title.objects.all()
     serializer_class = Title.Serializer
+
+
+class CommentViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+
+
+class ReviewViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
