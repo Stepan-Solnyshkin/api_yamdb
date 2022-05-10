34afaa1a360168b9184557086aec3c0a27c6e636
Dmitry Permyakov <taren4ik@mail.ru>
2022-05-10 21:55:39 +1000

    Добавил модели в reviews/admin.py

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Genre, Review, Comment, Title, Category
+
+
+class TitleAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "category",
+        "year",
+        "description",
+        "genre",
+    )
+
+    list_editable = ("genre",)
+    search_fields = ("description",)
+    list_filter = ("year",)
+    empty_value_display = "-пусто-"
+
+
+admin.site.register(Title, TitleAdmin)
+admin.site.register(Genre)
+admin.site.register(Comment)
+admin.site.register(Review)
+admin.site.register(Category)
