a7adcd41befb1422731766db193bfcbfc2499b67
Dmitry Permyakov <taren4ik@mail.ru>
2022-05-10 23:37:45 +1000

    Добавил модели в SlugRelatedField для сериализатора Review и viewset

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from rest_framework import filters, mixins, permissions, viewsets
 
 from reviews.models import Category, Comment, Genre, Title, Review
@@ -25,6 +26,17 @@ class TitleViewSet(viewsets.ReadOnlyModelViewSet):
     serializer_class = TitleSerializer
 
 
-class ReviewViewSet(viewsets.ReadOnlyModelViewSet):
-    queryset = Review.objects.all()
+class ReviewViewSet(viewsets.ModelViewSet):
+
     serializer_class = ReviewSerializer
+    permission_classes = []
+
+    def get_queryset(self):
+        title = get_object_or_404(Title, pk=self.kwargs.get("title_id"))
+        return title.reviews.all()
+
+    def perform_create(self, serializer):
+        title_id = self.kwargs.get('title_id')
+        title = get_object_or_404(Title, id=title_id)
+        serializer.save(author=self.request.user, title=title)
+
