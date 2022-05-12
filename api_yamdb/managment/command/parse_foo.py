cecf91831bd6126eb4441168b2bc60236ec33f6e
Dmitry Permyakov <taren4ik@mail.ru>
2022-05-12 23:38:30 +1000

    Залил таблицы в БД.

--- api_yamdb/managment/command/parse_foo.py
+++ api_yamdb/managment/command/parse_foo.py
@@ -6,4 +6,4 @@ import pandas
 connection = sqlite3.connect('db.sqlite3')
 
 df = pandas.read_csv('titles.csv', sep=',', header=0)
-df.to_sql('titles', connection, if_exists='append')
+df.to_sql('titles', connection, if_exists='append', index=False)
