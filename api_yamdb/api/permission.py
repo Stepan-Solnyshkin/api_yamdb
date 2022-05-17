e250ddc35ebe22c3470be906368ce22cfb559223
Dmitry Permyakov <taren4ik@mail.ru>
2022-05-17 22:53:55 +1000

    добавил пермишн IsAuthenticatedOrReadOnly для комментов и отзывов.

--- api_yamdb/api/permission.py
+++ api_yamdb/api/permission.py
@@ -1,5 +1,7 @@
 from rest_framework import permissions
 
+from users.models import User
+
 
 class AdminOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
@@ -44,3 +46,19 @@ class AdminOrReadOnly(permissions.BasePermission):
             return (request.method in permissions.SAFE_METHODS
                     or (request.user.is_authenticated and (
                         request.user.is_admin or request.user.is_superuser)))
+
+
+class IsReviewCommentPermissions(permissions.BasePermission):
+    """Пермишн для отзывов и комментов"""
+    def has_object_permission(self, request, view, obj):
+        if request.method == 'POST':
+            return not request.user.is_anonymous()
+
+        if request.method in ('PATCH', 'DELETE'):
+            return (request.user == obj.author or
+                    request.user.role == User.ADMIN or
+                    request.user.role == User.MODERATOR)
+
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return False
--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -9,7 +9,9 @@ from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters, mixins, status, viewsets
 from rest_framework.decorators import action, api_view, permission_classes
 from rest_framework.filters import OrderingFilter
-from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.permissions import (AllowAny, IsAuthenticated,
+                                        IsAuthenticatedOrReadOnly)
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import AccessToken
 from reviews.models import Category, Genre, Review, Title
@@ -17,7 +19,8 @@ from users.models import User
 
 from .filterset import TitleFilter
 from .permission import (AdminOnly, AdminOrReadOnly,
-                         IsAdminModeratorOwnerOrReadOnly, OnlyOwnAccount)
+                         IsAdminModeratorOwnerOrReadOnly, OnlyOwnAccount,
+                         IsReviewCommentPermissions)
 from .serializers import (CategorySerializer, CommentSerializer,
                           GenreSerializer, ReviewSerializer, SignUpSerializer,
                           TitleCreateSerializer, TitleSerializer,
@@ -27,7 +30,7 @@ from .serializers import (CategorySerializer, CommentSerializer,
 class CategoryViewSet(mixins.ListModelMixin,
                       mixins.CreateModelMixin,
                       mixins.DestroyModelMixin,
-                      viewsets.GenericViewSet,):
+                      viewsets.GenericViewSet, ):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
     permission_classes = [AdminOrReadOnly]
@@ -38,7 +41,8 @@ class CategoryViewSet(mixins.ListModelMixin,
 
 class CommentViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
-    permission_classes = [IsAdminModeratorOwnerOrReadOnly]
+    permission_classes = [IsReviewCommentPermissions,
+                          IsAuthenticatedOrReadOnly]
 
     def get_queryset(self):
         review = get_object_or_404(Review, pk=self.kwargs.get("review_id"))
@@ -79,7 +83,8 @@ class TitleViewSet(viewsets.ModelViewSet):
 
 class ReviewViewSet(viewsets.ModelViewSet):
     serializer_class = ReviewSerializer
-    permission_classes = [IsAdminModeratorOwnerOrReadOnly]
+    permission_classes = [IsReviewCommentPermissions,
+                          IsAuthenticatedOrReadOnly]
 
     def get_queryset(self):
         title = get_object_or_404(Title, pk=self.kwargs.get("title_id"))
