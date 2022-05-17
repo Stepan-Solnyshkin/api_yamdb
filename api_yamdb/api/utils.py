2e615aa84406f925dea386b04631ff12197bb523
Пименов Александр Витальевич <a.pimenov@avtodor-mc.ru>
2022-05-17 13:11:03 +0300

    README

---
+++ api_yamdb/api/utils.py
@@ -0,0 +1,13 @@
+from django.shortcuts import get_object_or_404
+from reviews.models import Title
+
+
+class CurrentTitleDefault:
+    requires_context = True
+
+    def __call__(self, serializer_field):
+        title_id = serializer_field.context['view'].kwargs.get('title_id')
+        return get_object_or_404(Title, id=title_id)
+
+    def __repr__(self):
+        return '%s()' % self.__class__.__name__
