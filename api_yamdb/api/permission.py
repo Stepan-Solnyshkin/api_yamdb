fa2df9d8e3ee3e66f4b90892e7b866198f453c0d
Dmitry Permyakov <taren4ik@mail.ru>
2022-05-20 00:07:44 +1000

    rev.2

--- api_yamdb/api/permission.py
+++ api_yamdb/api/permission.py
@@ -17,20 +17,20 @@ class OnlyOwnAccount(permissions.BasePermission):
         return obj.user == request.user
 
 
+class ReadOnly(permissions.BasePermission):
+    """Проверка только на чтение."""
+    def has_permission(self, request, view):
+        return request.method in permissions.SAFE_METHODS
+
+
 class IsAdminModeratorOwnerOrReadOnly(permissions.BasePermission):
-    """Класс проверяет, что пользователь является модератором.
-    """
+    """Класс проверяет, что пользователь является модератором, автором или
+    админом"""
 
     def has_object_permission(self, request, view, obj):
         return (request.method in permissions.SAFE_METHODS
                 or request.user.is_admin or request.user.is_moderator
                 or obj.author == request.user)
-
-
-class ReadOnly(permissions.BasePermission):
-    """Проверка только на чтение."""
-    def has_permission(self, request, view):
-        return (request.method in permissions.SAFE_METHODS)
 
 
 AdminOrReadOnly = ReadOnly | AdminOnly
